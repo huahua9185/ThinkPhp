c672bec41c974459cab20e60a7b6c08ab6bbf133
huahua9185 <huahua9185@gmail.com>
2016-02-24 23:35:38 +0800

    20160224
    password和confirmpass判断总是不对，找不到原因。太晚了，睡了，明天再说吧

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,13 +6,38 @@ class IndexController extends CommonController {
     	$this->display();
 
     }
-    public  function  showuser(){
+    //显示用户
+    public  function  users(){
         $user=M('user');
         $result=$user->select();
-        var_dump($result);
+        //var_dump($result);
         $this->assign('list',$result);
-        $this->display('show_user');
+        $this->display('');
 
 
+    }
+    //增加用户
+    public  function  users_add(){
+        //获取用户组，供前台select列表使用
+        $grouplist=M('role');
+        $result=$grouplist->select();
+        $this->assign('group',$result);
+        //增加用户
+        //首先对提交的数据进行判断
+        if(IS_POST){
+            var_dump($_POST);
+          //前台post的数据username,password,confirm_password,nickname,email,type
+            if(I('post.username')==null){
+                $this->error('用户名不能为空',U('Home/Index/users_add'));
+            }
+            if(I('post.password')==null||I('post.confirm_password')==null){
+                $this->error('密码不能为空',U('Home/Index/users_add'));
+            }
+            if(I('post.password')!=I('post.confirm.password')){
+                    $this->success('两次输入的密码不一致',U('Home/Index/users_add'));
+                }
+        }
+
+        $this->display();
     }
 }
--- Application/Runtime/Cache/Home/a006f4400e0114cacd415489be5e5b85.php
+++ Application/Runtime/Cache/Home/a006f4400e0114cacd415489be5e5b85.php
@@ -46,10 +46,13 @@
 		      </div>
 		    </nav>  			
   		</div>
+	  <div class="clearfix"></div>
   		<div class="row">
-		        <div class="col-sm-3 col-md-2 sidebar">
+		        <div class="col-md-2 sidebar">
 		          <ul class="nav nav-sidebar">
-		            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
+		            <li
+							class="active"><a href="<?php echo U('Home/Index/users');?>">用户管理
+						<span class="sr-only">(current)</span></a></li>
 		            <li><a href="#">Reports</a></li>
 		            <li><a href="#">Analytics</a></li>
 		            <li><a href="#">Export</a></li>
@@ -67,8 +70,54 @@
 		            <li><a href="">Another nav item</a></li>
 		          </ul>
 		        </div>
-		        <div class="col-sm-9 col-md-10 main">
-		        </div>
+
+				<div class="col-md-8 col-md-offset-2">
+		        
+    <div class="panel panel-primary top">
+        <div class="panel-heading">添加用户</div>
+        <div class="panel-body">
+            <form action="/thinkphp/Home/Index/users_add.html" method="post" class="form-horizontal">
+                <div class="form-group">
+                    <label for="username" class="col-sm-2 control-label">用户名:</label>
+                    <div class="col-sm-2"><input type="text" class="form-control" id="username" name="username"></div>
+                </div>
+                <div class="form-group">
+                    <label for="password" class="col-sm-2 control-label">密码:</label>
+                    <div class="col-sm-2">
+                        <input type="password" class="form-control" id="password" name="password"></div>
+                </div>
+                <div class="form-group">
+                    <label for="confirm_password" class="col-sm-2 control-label">确认密码:</label>
+                    <div class="col-sm-2">
+                        <input type="password" class="form-control" id="confirm_password" name="confirm_password"></div>
+                </div>
+                <div class="form-group">
+                    <label for="nickname" class="col-sm-2 control-label">昵称:</label>
+                    <div class="col-sm-2"> <input type="text" class="form-control" id="nickname" name="nickname"></div>
+                </div>
+                <div class="form-group">
+                    <label for="email" class="col-sm-2 control-label">邮箱:</label>
+                    <div class="col-sm-2"> <input type="text" class="form-control" id="email" name="email"></div>
+                </div>
+                <div class="form-group">
+                    <label class="col-sm-2 control-label">账户类型</label>
+                    <div class="col-sm-2">
+                        <select class="form-control" name="type">
+                            <?php if(is_array($group)): $i = 0; $__LIST__ = $group;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$vo): $mod = ($i % 2 );++$i;?><option value="<?php echo ($vo["id"]); ?>"><?php echo ($vo["id"]); ?>.<?php echo ($vo["name"]); ?></option><?php endforeach; endif; else: echo "" ;endif; ?>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-2 col-sm-offset-2"><button type="submit" name="user_add"
+                                                                  class="btn btn-warning">增加用户
+                    </button></div>
+
+                </div>
+            </form>
+        </div>
+    </div>
+
+				</div>
   		</div>
   </div>
 
